a676474bdcb5230845d2244219448cbed4681f9b
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 15:09:51 +0530

    Add project files.

---
+++ AllowAnonymousDemo/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllowAnonymousDemo.Controllers
+{
+    [AllowAnonymous]
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ AllowAnonymousDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllowAnonymousDemo.MvcApplication" Language="C#" %>
